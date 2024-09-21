7b77c471683e357c947a739da196e4ffeb290028
Robson William <30180338+robwillup@users.noreply.github.com>
2024-09-21 09:09:25 -0300

    Making the cards tapable

--- src/Home.tsx
+++ src/Home.tsx
@@ -49,52 +49,22 @@ const Home: React.FC = () => {
     <>
       {isHome && (
         <Flex direction={isMobile ? "column" : "row"} gap={4}>
-          <Card maxW="xl">
-            {isMobile ? (
-              <Button
-                onClick={handleCardGoStats} // Make card tapable on mobile
-                width="100%"
-                height="100%"
-                p={0} // Remove default padding
-                variant="ghost" // Make button look like it's not a button
-                _hover={{ bg: "transparent" }} // No hover effect
-                _focus={{ boxShadow: "none" }} // No focus outline
-                _active={{ bg: "transparent" }} // No active effect
-                display="block" // Ensure it takes the full card space
-              >
-                <CardBody>
-                  <Image
-                    src="https://images.unsplash.com/photo-1666875753105-c63a6f3bdc86?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                    alt="Transaction Stats"
-                    borderRadius="lg"
-                  />
-                  <Stack mt="6" spacing="3">
-                    <Heading size="md">Purchase Insights</Heading>
-                    <Text fontSize={20}>
-                      Explore detailed statistics and trends from recent
-                      transactions to gain valuable insights into customer
-                      behavior and sales performance.
-                    </Text>
-                  </Stack>
-                </CardBody>
-              </Button>
-            ) : (
-              <CardBody>
-                <Image
-                  src="https://images.unsplash.com/photo-1666875753105-c63a6f3bdc86?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-                  alt="Transaction Stats"
-                  borderRadius="lg"
-                />
-                <Stack mt="6" spacing="3">
-                  <Heading size="md">Purchase Insights</Heading>
-                  <Text fontSize={20}>
-                    Explore detailed statistics and trends from recent
-                    transactions to gain valuable insights into customer
-                    behavior and sales performance.
-                  </Text>
-                </Stack>
-              </CardBody>
-            )}
+          <Card maxW="xl" onClick={isMobile ? handleCardGoStats : undefined}>
+            <CardBody>
+              <Image
+                src="https://images.unsplash.com/photo-1666875753105-c63a6f3bdc86?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="Transaction Stats"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">Purchase Insights</Heading>
+                <Text fontSize={20} noOfLines={2}>
+                  Explore detailed statistics and trends from recent
+                  transactions to gain valuable insights into customer behavior
+                  and sales performance.
+                </Text>
+              </Stack>
+            </CardBody>
             {!isMobile && (
               <CardFooter justifyContent="center">
                 <ButtonGroup spacing="2">
@@ -104,6 +74,36 @@ const Home: React.FC = () => {
                     colorScheme="blue"
                   >
                     Go to Statistics
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            )}
+          </Card>
+
+          <Card maxW="xl" onClick={isMobile ? handleCardGoList : undefined}>
+            <CardBody>
+              <Image
+                src="https://images.unsplash.com/photo-1616077167555-51f6bc516dfa?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                alt="Transaction List"
+                borderRadius="lg"
+              />
+              <Stack mt="6" spacing="3">
+                <Heading size="md">Purchase Overview</Heading>
+                <Text fontSize={20} noOfLines={2}>
+                  Browse through all transactions with flexible filtering and
+                  sorting options to easily manage and review purchases.
+                </Text>
+              </Stack>
+            </CardBody>
+            {!isMobile && (
+              <CardFooter justifyContent="center">
+                <ButtonGroup spacing="2">
+                  <Button
+                    onClick={handleGoList}
+                    variant="solid"
+                    colorScheme="blue"
+                  >
+                    Go to List
                   </Button>
                 </ButtonGroup>
               </CardFooter>
