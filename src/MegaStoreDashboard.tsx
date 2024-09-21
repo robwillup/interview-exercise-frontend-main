d62c7426e657344d6bc97b0f582c593eaf418fc3
Robson William <30180338+robwillup@users.noreply.github.com>
2024-09-21 08:45:48 -0300

    Hiding home card text when mobile

--- src/MegaStoreDashboard.tsx
+++ src/MegaStoreDashboard.tsx
@@ -5,7 +5,6 @@ import {
   CardBody,
   CardFooter,
   Image,
-  Divider,
   Heading,
   Stack,
   Text,
@@ -34,11 +33,23 @@ const MegaStoreDashboard: React.FC = () => {
     setIsStats(true);
   };
 
+  const handleCardGoStats = () => {
+    if (isMobile) {
+      handleGoStats();
+    }
+  };
+
+  const handleCardGoList = () => {
+    if (isMobile) {
+      handleCardGoList();
+    }
+  };
+
   return (
     <>
       {isHome && (
         <Flex direction={isMobile ? "column" : "row"} gap={4}>
-          <Card maxW="xl">
+          <Card maxW="xl" onClick={handleCardGoStats}>
             <CardBody>
               <Image
                 src="https://images.unsplash.com/photo-1666875753105-c63a6f3bdc86?q=80&w=2073&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -54,20 +65,21 @@ const MegaStoreDashboard: React.FC = () => {
                 </Text>
               </Stack>
             </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2" pl={200}>
-                <Button
-                  onClick={handleGoStats}
-                  variant="solid"
-                  colorScheme="blue"
-                >
-                  Go to Statistics
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
+            {!isMobile && (
+              <CardFooter>
+                <ButtonGroup spacing="2" pl={200}>
+                  <Button
+                    onClick={handleGoStats}
+                    variant="solid"
+                    colorScheme="blue"
+                  >
+                    Go to Statistics
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            )}
           </Card>
-          <Card maxW="xl">
+          <Card maxW="xl" onClick={handleCardGoList}>
             <CardBody>
               <Image
                 src="https://images.unsplash.com/photo-1616077167555-51f6bc516dfa?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -82,18 +94,19 @@ const MegaStoreDashboard: React.FC = () => {
                 </Text>
               </Stack>
             </CardBody>
-            <Divider />
-            <CardFooter>
-              <ButtonGroup spacing="2" pl={200}>
-                <Button
-                  onClick={handleGoList}
-                  variant="solid"
-                  colorScheme="blue"
-                >
-                  Go to List
-                </Button>
-              </ButtonGroup>
-            </CardFooter>
+            {!isMobile && (
+              <CardFooter>
+                <ButtonGroup spacing="2" pl={200}>
+                  <Button
+                    onClick={handleGoList}
+                    variant="solid"
+                    colorScheme="blue"
+                  >
+                    Go to List
+                  </Button>
+                </ButtonGroup>
+              </CardFooter>
+            )}
           </Card>
         </Flex>
       )}
